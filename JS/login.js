745e98a1c58fc2f887ee6bb3612f9fc39411a945
Almaz <almaz.esengulov.pi9.03.20@gmail.com>
2023-03-11 23:36:03 +0600

    feat: add user

---
+++ JS/login.js
@@ -0,0 +1,24 @@
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+const signIn = document.querySelector(".signIn");
+const error = document.querySelector(".error");
+
+const allUser = JSON.parse(localStorage.getItem("users"));
+
+signIn.addEventListener("click", e => {
+    e.preventDefault();
+    const user = allUser.find(item => item.email === email.value);
+
+    console.log(user);
+    if (email.value !== "" && password.value !== "") {
+        if (user) {
+            localStorage.setItem("user", JSON.stringify(user));
+            localStorage.setItem("access_token", "true");
+            window.open("../index.html", "_self");
+        } else {
+            error.innerHTML = "Данный пользователь не существует!"
+        }
+    } else {
+        error.innerHTML = "Все поля должны быть заполнены!";
+    }
+});
---
+++ JS/register.js
@@ -0,0 +1,45 @@
+const lastName = document.querySelector(".lastName");
+const fIrstName = document.querySelector(".fIrstName");
+const email = document.querySelector(".email");
+const img = document.querySelector(".img");
+const password = document.querySelector(".password");
+const error = document.querySelector(".error");
+const signUp = document.querySelector(".signUp");
+
+window.addEventListener("load", () => {
+    if (!localStorage.getItem("users")) {
+        localStorage.setItem("users", JSON.stringify([]));
+    }
+});
+
+const allUsers = JSON.parse(localStorage.getItem("users"));
+
+signUp.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const user = !!allUsers.find(item => item.email === email.value);
+    console.log(user);
+
+    if (lastName.value !== "" && fIrstName.value !== "" && email.value !== "" && password.value !== "" && img.value !== "") {
+        if (!user) {
+            localStorage.setItem("users", JSON.stringify([
+                ...allUsers,
+                {
+                    lastName: lastName.value,
+                    fIrstName: fIrstName.value,
+                    email: email.value,
+                    img: img.value,
+                    password: password.value
+                }
+            ]))
+            window.open("../login.html", "_self");
+        } else {
+            error.innerHTML = "Данный пользователь существует!";
+        }
+        email.value = "";
+        password.value = "";
+        name.value = "";
+    } else {
+        error.innerHTML = "Все поля должны быть заполнены!";
+    }
+});
